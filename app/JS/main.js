42aac8b03233bc0c00a86b5703453e67e1bf486a
zongzhili <zongzhil@nycstudents.net>
2024-12-16 14:00:45 -0500

    wer

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,1 +1,19 @@
 import "/CSS/style.css";
+
+let board = Array(6)
+  .fill()
+  .map(() => Array(7).fill(null));
+console.log(board);
+
+let currentPlayer = "player1";
+
+function createBoard() {
+  for (i = 0; i < 6; i++) {
+    for (i = 0; i < 7; i++) {
+      const board = (spaces) => `
+            <div class="board">
+
+            </div>`;
+    }
+  }
+}
