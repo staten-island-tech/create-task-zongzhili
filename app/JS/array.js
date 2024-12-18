1ff35bac95c2769fe25e35fab5a8277d8b136714
zongzhili <zongzhil@nycstudents.net>
2024-12-18 14:01:22 -0500

    dfdfg

---
+++ app/JS/array.js
@@ -0,0 +1,4 @@
+let board = Array(6)
+  .fill()
+  .map(() => Array(7).fill(null));
+console.log(board);
--- app/JS/main.js
+++ app/JS/main.js
@@ -1,14 +1,36 @@
 import "/CSS/style.css";
 
-let board = Array(6)
-  .fill()
-  .map(() => Array(7).fill(null));
-console.log(board);
-
-let currentPlayer = "player1";
+const playerTurnElement = document.getElementById("player-turn");
+const turnButton = document.getElementById("turn-button");
 
 function createBoard() {
   for (i = 0; i < 6; i++) {
     for (i = 0; i < 7; i++) {}
   }
 }
+
+const players = ["Player 1", "Player 2"];
+
+let currentPlayerIndex = 0;
+
+function changeTurn() {
+  currentPlayerIndex = (currentPlayerIndex + 1) % players.length; //alternate p1 & p2
+  playerTurnElement.insertAdjacentHTML(
+    "beforebegin",
+    "<h2>Player ${currentPlayerIndex}</h2>"
+  );
+}
+turnButton.addEventListener("click", changeTurn);
+
+// document.querySelector("buttons").addEventListener("click", function () {
+//   //yellow and red token drop
+//   if (document.body.classList.contains("cold")) {
+//     document.body.classList.add("hot");
+//     document.body.classList.remove("cold");
+//   } else {
+//     document.body.classList.add("cold");
+//     document.body.classList.remove("hot");
+//   }
+// });
+
+turnButton.querySelector.innerHTML("");
