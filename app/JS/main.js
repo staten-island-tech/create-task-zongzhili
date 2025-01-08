f6d92e1af23338ee19f2d5df147ee04746d6b13f
zongzhili <zongzhil@nycstudents.net>
2025-01-08 14:01:10 -0500

    aa

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,51 +1,57 @@
 import "/CSS/style.css";
 
 const players = [
-  { name: "player1", score: 0 },
-  { name: "player2", score: 0 },
+  { name: "Player 1", score: 0 },
+  { name: "Player 2", score: 0 },
 ];
-const winScore = 150;
+const winScore = 100;
 let currentPlayerIndex = 0;
 
 const result = document.getElementById("result");
 const roll = document.getElementById("roll-button");
 
-//while (players[0].score < winScore && players[0].score < winScore) {}
-
-roll.addEventListener("click", function () {
-  //for (let turn = 0; turn < 40; turn++) {
-  document.getElementById("result").innerHTML = "";
-
+function rollDice() {
   const num1 = Math.floor(Math.random() * 6) + 1;
   const num2 = Math.floor(Math.random() * 6) + 1;
+  return [num1, num2];
+}
+roll.addEventListener("click", function () {
+  //add iteration and parameters
+  //for (let turn = 0; turn < 30; turn++) {
+  document.getElementById("result").innerHTML = "";
 
+  const [num1, num2] = rollDice();
   const currentPlayer = players[currentPlayerIndex];
 
   if (num1 === num2) {
     const result = document.getElementById("result");
     result.insertAdjacentHTML(
       "beforeend",
-      `<p>You rolled: ${num1} and ${num2} (YAY)</p>`
+      `<h3>${currentPlayer.name} rolled: ${num1} and ${num2} (YAY)</h3>`
     );
     currentPlayer.score += 20;
   } else {
     result.insertAdjacentHTML(
       "beforeend",
-      `<p>You rolled: ${num1} and ${num2}</p>`
+      `<h3>${currentPlayer.name} rolled: ${num1} and ${num2}</h3>`
     );
     currentPlayer.score += num1 + num2;
   }
   console.log(`${currentPlayer.name}'s score: ${currentPlayer.score}`);
+  result.insertAdjacentHTML(
+    "beforeend",
+    `<h3> ${currentPlayer.name}'s score: ${currentPlayer.score}</h3>`
+  );
 
   if (currentPlayer.score >= winScore) {
     result.insertAdjacentHTML(
       "beforeend",
       `<p>${currentPlayer.name} wins with ${currentPlayer.score} points!</p>`
     );
-    // rollButton.disabled = true;
-    //return;
+    rollButton.disabled = true;
+    return;
   }
-  currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+  currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
   // if (turn === 39) {
   //   result.insertAdjacentHTML(
   //     "beforeend",
@@ -55,7 +61,7 @@ roll.addEventListener("click", function () {
   // }
   //}
 });
-
+function gameOver() {}
 // function changeTurn(players) {
 //   currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
 // }
